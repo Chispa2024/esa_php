4eaa4f945773483a34c8f43184c3522a2c5e1645
Chispa2024 <gabriela.pena.perez@gmail.com>
2024-04-19 13:11:20 +0200

    Create index.php

---
+++ palindrome/index.php
@@ -0,0 +1,40 @@
+<?
+
+/**
+ * @author Gabriela <gabriela.pena.perez@gmail.com>
+ * @copyright 2024 GaP
+ *
+ */
+
+// lit le mot en ligne de commande
+$mot = readline("Entrer un mot: ");
+
+/**
+ * Palindrome function
+ *
+ * @param [string] $mot
+ * @return void
+ */
+
+function estPalindrome($mot)
+{
+    // convertir le mot en minuscule et supprimer les espaces
+    $mot = strtolower(str_replace(' ', '', $mot));
+
+    // Longueur du mot
+    $longueur = strlen($mot);
+
+    // Vérifier si le mot est un palindrome
+    for ($i = 0; $i < $longueur / 2; $i++) {
+        if ($mot[$i] != $mot[$longueur - 1 - $i]) {
+            return false;
+        }
+    }
+
+
+    // Le mot est un palindrome
+    return true;
+}
+
+//affiche comme résultat
+echo $mot . " est-il un palindrome ? : " . (estPalindrome($mot) ? "Oui" : "Non") . "\n";
